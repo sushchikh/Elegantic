41fac7c0ff0e1118205a28eed54eb881a359d5e8
sushchikh <a.sushchikh@gmail.com>
2017-04-02 10:48:54 +0300

    carousel add

--- js/my.js
+++ js/my.js
@@ -1,5 +1,7 @@
 window.onload = function(){
 
+
+//бургер-меню
 	var burger = document.getElementById('burger');
 	var menu = document.getElementById('menu_desctop');
 	burger.onclick = addShowClass;
@@ -7,6 +9,22 @@ window.onload = function(){
 	function addShowClass(){
 		menu.classList.toggle('menu_show');
 	};
+
+
+// карусель
+	var carousels = document.querySelectorAll('#carousel_container .carousel_element');
+	var currentCarousel = 0;
+	var carouselInteraval = setInterval(nextCarousel, 5000);
+
+	function nextCarousel(){
+		carousels[currentCarousel].className = 'carousel_element';
+		currentCarousel = (currentCarousel+1) % carousels.length;
+		carousels[currentCarousel].className = 'carousel_element show'
+	};
+
+
+
+
 
 
 };//window.onload function end
